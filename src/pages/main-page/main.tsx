ef3b638a2b5ed0833c69353ecee9df0221ab8dbf
compotee <mp795610@gmail.com>
2026-01-05 22:47:20 +0500

    Initial commit

---
+++ src/pages/main-page/main.tsx
@@ -0,0 +1,29 @@
+import "./main-style.css"
+
+import teamImage from '../../assets/main-page-img.svg';
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="logo-div">
+        <img src={logo} alt="Team Builder" />
+        <h1>Team Builder</h1>
+      </div>
+      <h2>Сформируй full-stack команду</h2>
+      <p>
+        TeamBuilder — это система, в которой каждый участник указывает свои навыки, предпочтительные роли и уровень опыта. Алгоритм платформы автоматически распределяет людей по командам, обеспечивая оптимальный баланс компетенций и ролей в каждой группе для успешного выполнения проекта.
+        <br/>
+        <br/>
+        Наша цель - автоматизировать формирование сбалансированных и эффективных проектных команд на основе компетенций и ролей участников.
+      </p>
+      {/* <div>
+        <button>Вход</button>
+        <button>Регестрация</button>
+      </div> */}
+      <img src={teamImage} alt="Изображение команды" />
+    </div>
+  );
+};
+
+export default Home;
