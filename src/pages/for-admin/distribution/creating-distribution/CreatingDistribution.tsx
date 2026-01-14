09b9692e3008bd7672e7225d3f717e229f8c6c7b
compotee <mp795610@gmail.com>
2026-01-14 21:58:30 +0500

    feat: joined members page

--- src/pages/for-admin/distribution/creating-distribution/CreatingDistribution.tsx
+++ src/pages/for-admin/distribution/creating-distribution/CreatingDistribution.tsx
@@ -4,7 +4,30 @@ import "./CreatingDistribution.css"
 const CreatingDistribution = () => {
     return (
         <div>
-            Создать распределение
+            <p>1. Заполните данные о распределении</p>
+            <div>
+                <span></span>
+                <input type="text" />
+            </div>
+            <div>
+                <span></span>
+                <input type="number" />
+            </div>
+            <p>2.  Выберите роли, которые необходимы в проекте</p>
+            <p>3. Заполните данные о временных метриках распределения</p>
+            <div>
+                <span></span>
+                <input type="text" />
+            </div>
+            <div>
+                <span></span>
+                <input type="number" />
+            </div>
+            <div>
+                <span></span>
+                <input type="number" />
+            </div>
+            <button className="button button--inactive-pending">Запустить распределение</button>
         </div>
     );
 };
--- src/pages/for-admin/distribution/joined-members/JoinedMembers.tsx
+++ src/pages/for-admin/distribution/joined-members/JoinedMembers.tsx
@@ -1,9 +1,29 @@
 import "./JoinedMembers.css"
 
+import userIcon from '../../../../assets/user-in-team-icon.svg'
+
 
 const JoinedMembers = () => {
     return (
-        <>Присоеденившиеся участники</>
+        <div className="joined-members-container">
+            <h2 className="joined-members-container_title">Присоединившиеся участники</h2>
+            <div className="number-of-memmbers">
+                <span className="number-of-memmbers_text">Количество присоединившихся участников </span>
+                {/* длина списка участников */}
+                <div className="number-of-memmbers_count">10</div>
+            </div>
+            {/* тут участники через if */}
+            {Array.from({ length: 15 }, (_, index) => (
+                <div key={index} className="member-in-team">
+                    <img src={userIcon} alt="Иконка участника" />
+                    <p className="member-in-team_name">Петров Петр Петрович</p>
+                    <div className="member-in-team_competencies">
+                    <div className="member-in-team_competencies-item">Аналитик</div>
+                    <div className="member-in-team_competencies-item">Дизайнер</div>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 };
 
